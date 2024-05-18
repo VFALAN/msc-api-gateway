eb11fab8778d3f2a2b0c2118a27e26a82066c2d7
VFALAN <alanvillafan95@gmail.com>
2024-05-18 05:14:05 -0600

    first commit

---
+++ src/main/java/com/msc/gw/mscapigateway/MscApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.msc.gw.mscapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msc/gw/mscapigateway/MscApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msc.gw.mscapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
